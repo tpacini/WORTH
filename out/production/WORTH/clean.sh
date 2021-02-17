2b37ff136ec4535e3df7323a6020715751c31e1a
r4nxer0x <teodorico.pacini@gmail.com>
2021-02-17 23:12:11 +0100

    [FIX] Readme and PDF files

---
+++ out/production/WORTH/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ $(ls Client/ | wc -l) -gt 4 ]; then rm Client/*.class; fi
+if [ $(ls Server/ | wc -l) -gt 5 ]; then rm Server/*.class; fi
+if [ $(ls Commons/ | wc -l) -gt 4 ]; then rm Commons/*.class; fi 
---
+++ out/production/WORTH/compile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+#clean .class files created previously
+./clean.sh
+
+if [ $? -eq 0 ]
+then
+  echo "[OK] Old .class files deleted."
+else
+  echo "[ERROR] Unable to delete old .class files." >&2
+  exit 1
+fi
+
+#compile the project with all the external libraries used
+javac -cp ../json-simple-1.1.jar \
+      -d . \
+           Server/AdvKey.java \
+	   Server/Card.java \
+	   Server/Project.java \
+	   Server/DBMS.java \
+	   Server/ServerMain.java \
+	   Client/ClientNotifyInterface.java \
+	   Client/MulticastInfos.java \
+	   Client/ClientNotifyImpl.java \
+	   Client/ClientMain.java \
+	   Commons/RMICallbackImpl.java \
+	   Commons/RMICallbackInterface.java \
+	   Commons/RMIRegistrationImpl.java \
+  	   Commons/RMIRegistrationInterface.java
+
+if [ $? -eq 0 ]
+then
+  echo "[OK] Successfully compiled!"
+  #exit 0
+else
+  echo "[ERROR] Unable to compile the project." >&2
+  #exit 1
+fi
